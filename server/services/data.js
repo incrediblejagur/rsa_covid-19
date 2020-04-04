4265c8ff005eff397fbd9aeac1010e546a3142f8
Daniel <danielminter13@gmail.com>
2020-04-04 13:25:12 +0200

    updated past data

--- server/services/data.js
+++ server/services/data.js
@@ -1,124 +1,184 @@
+//past data, before we started capturing data on COVID-19 in South Africa
+//Source: https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data/South_Africa_medical_cases_chart
 module.exports = data =    
     [
         {
             date: '2020/03/05',
-            cases: 1
+            cases: 1,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/06',
-            case: 1
+            cases: 1,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/07',
-            cases: 2
+            cases: 2,
+            recoveries:0,
+            deaths:0
         },
 
         {
             date: '2020/03/08',
-            cases: 3
+            cases: 3,
+            recoveries:0,
+            deaths:0
         },
 
         {
             date: '2020/03/09',
-            cases: 7
+            cases: 7,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/10',
-            cases: 7
+            cases: 7,
+            recoveries:0,
+            deaths:0
         },
 
         {
             date: '2020/03/11',
-            cases: 13
+            cases: 13,
+            recoveries:0,
+            deaths:0
         },
 
         {
             date: '2020/03/12',
-            cases: 16
+            cases: 16,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/13',
-            cases: 24
+            cases: 24,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/14',
-            cases: 38
+            cases: 38,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/15',
             cases: 51,
-            source: "announced by president Ramaphosa later in the evening.}}51;+34%"
+            source: "announced by president Ramaphosa later in the evening.}}51;+34%",
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/16',
-            cases: 62
+            cases: 62,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/17',
-            cases: 85
+            cases: 85,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/18',
-            cases: 116
+            cases: 116,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/19',
-            cases: 150
+            cases: 150,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/20',
-            cases: 202
+            cases: 202,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/21',
-            cases: 240
+            cases: 240,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/22',
-            cases: 274
+            cases: 274,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/23',
-            cases: 402
+            cases: 402,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/24',
-            cases: 554
+            cases: 554,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/25',
-            cases: 709
+            cases: 709,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/26',
-            cases: 927
+            cases: 927,
+            recoveries:0,
+            deaths:0
         },
         {
             date: '2020/03/27',
-            cases: 1170
+            cases: 1170,
+            recoveries:31,
+            deaths:1
         },
         {
             date: '2020/03/28',
-            cases: 1187
+            cases: 1187,
+            recoveries:31,
+            deaths:1
         },
         {
             date: '2020/03/29',
-            cases: 1280
+            cases: 1280,
+            recoveries:31,
+            deaths:2
         },
         {
             date: '2020/03/30',
-            cases: 1326
+            cases: 1326,
+            recoveries:31,
+            deaths:3
         },
         {
             date: '2020/03/31',
-            cases: 1353
+            cases: 1353,
+            recoveries:31,
+            deaths:5
         },
         {
             date: '2020/04/01',
-            cases: 1380
+            cases: 1380,
+            recoveries:31,
+            deaths:5
         },
         {
             date: '2020/04/02',
-            cases: 1462
+            cases: 1462,
+            recoveries:0,
+            deaths:5
         }
     ]
