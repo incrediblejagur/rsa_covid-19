f20afb77c76558b1d2d74f0b49ed285f3d8030fd
Daniel <danielminter13@gmail.com>
2020-04-15 23:04:33 +0200

    now pulls extra stats from server

--- client/src/components/extra_stats.jsx
+++ client/src/components/extra_stats.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import axios from 'axios';
 
 
 export default class ExtraStats extends React.Component{
+    state = {
+        latestIncrease:'',
+        averageIncrease:'',
+        lastUpdated:''
+    }
+    componentDidMount = () => {
+        axios.get('/api/extra-info')
+            .then((result) => {
+                let data = result.data.data
+                this.setState({
+                    latestIncrease:data.latest_increase,
+                    averageIncrease: data.average_increase
+                })
+                
+            })
+            axios.get('/api/latest-stats')
+            .then((result) => {
+                let data = result.data.data
+                this.setState({lastUpdated:data[4].time})           
+            })
+        }
 
     render(){
+        let {latestIncrease, averageIncrease, lastUpdated} = this.state;
         return(
               <div className='col-6' style={{marginTop:30}}>
                             <div className='row'>
@@ -12,13 +34,13 @@ export default class ExtraStats extends React.Component{
                         <div className="card" style={{ width: '18rem' }}>
                     <div className="card-body">
                         <h5 className="card-title">Latest Increase</h5>
-                        <p className="card-text">230</p>
+        <p className="card-text">{latestIncrease}</p>
                     </div>
                 </div>
                 <div className="card" style={{ width: '18rem' }}>
                     <div className="card-body">
                         <h5 className="card-title">Average Increase</h5>
-                        <p className="card-text">230</p>
+        <p className="card-text">{averageIncrease}</p>
                     </div>
                 </div>
                 </div>
@@ -27,14 +49,14 @@ export default class ExtraStats extends React.Component{
                         <div className="card-group" style={{ marginTop: 0 }}>
                         <div className="card" style={{ width: '18rem' }}>
                     <div className="card-body">
-                        <h5 className="card-title">Days in Lockdown</h5>
-                        <p className="card-text">#</p>
+                        <h5 className="card-title">For more info</h5>
+                        <p className="card-text"><a href="https://sacoronavirus.co.za/">sacoronavirus.co.za</a></p>
                     </div>
                 </div>
                 <div className="card" style={{ width: '18rem' }}>
                     <div className="card-body">
                         <h5 className="card-title">Last Updated</h5>
-                        <p className="card-text">26/05/2020 14:45</p>
+        <p className="card-text">{lastUpdated}</p>
                     </div>
                 </div>
                 </div>
