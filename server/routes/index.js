db6fa698fc01ad5d61041a2561fb99792cd66dd4
Daniel <danielminter13@gmail.com>
2020-04-15 23:04:10 +0200

    updated urls, and aded extrainfo api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 module.exports = (app,data_api) => {
     
-    app.get('/api/covid/latest', data_api.latestStatistics);
-    app.get('/api/covid/province', data_api.statsByProvince)
-    app.get('/api/covid/all',data_api.collectedCovidState)
-    app.get('/api/covid/geojson',data_api.geoJSONdata)
+    app.get('/api/latest-stats', data_api.latestStatistics);
+    app.get('/api/extra-info', data_api.extraCovidInfo)
+    app.get('/api/province-stats', data_api.statsByProvince)
+    app.get('/api/all-stats',data_api.collectedCovidState)
+    app.get('/api/geojson',data_api.geoJSONdata)
 
 
     
