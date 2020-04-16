48557f2b11adb645abe387ee0de9f86e54c721c2
Daniel <danielminter13@gmail.com>
2020-04-16 11:26:14 +0200

    add basic footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class Footer extends React.Component {
+
+    render() {
+        return (
+            <div style={{textAlign:"center", backgroundColor:"#343a40", padding:5}}>
+                <code style={{fontSize: 17, color: "white"}}>Made by Daniel Minter
+                </code>
+                </div>
+        )
+    }
+}
