2ba4bdcf0e1ec2c634834132b1eb09c99ec042c7
Daniel <danielminter13@gmail.com>
2020-04-17 21:47:00 +0200

    update react build

---
+++ client/build/precache-manifest.6a9508ae2466b8824df4b7a5acc758e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f870a2dd3dbee7e3c067571350d584f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b69bbbd6fd8162e5b51",
+    "url": "/static/css/main.d2796dfe.chunk.css"
+  },
+  {
+    "revision": "64494714e9e677fa880c",
+    "url": "/static/js/2.3fa3e591.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.3fa3e591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b69bbbd6fd8162e5b51",
+    "url": "/static/js/main.e9df94c7.chunk.js"
+  },
+  {
+    "revision": "d2929e1c58fcc6423ed0",
+    "url": "/static/js/runtime-main.c978f367.js"
+  }
+]);
