c79c2812c382783d71417bcc888043cc3d4f9e72
Daniel <danielmntr@protonmail.com>
2020-04-28 18:05:03 +0200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 module.exports = (app,data_api) => {
     
     app.get('/api/latest-stats', data_api.latestStatistics);
+    app.get('/api/covid/all', data_api.latestStatistics);
     app.get('/api/extra-stats', data_api.extraCovidStats)
     app.get('/api/province-stats', data_api.statsByProvince)
     app.get('/api/all-stats',data_api.collectedCovidState)
