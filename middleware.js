2d7a0d20b99a4b7dd47415d061962496e410bc3f
Daniel <danielminter13@gmail.com>
2020-05-01 14:30:16 +0200

    fix middleware bug

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports = () => {
 
 const useSecureProtocol = (req,res,next) => {
-    if(req.header['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production'){
+    if(req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production'){
         return res.redirect('https://'+req.headers.host+req.url)
     }else{
         return next();
