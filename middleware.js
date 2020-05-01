873b8650e53c53ffe8dbae91ee56ee926694fa48
Daniel <danielminter13@gmail.com>
2020-05-01 15:04:53 +0200

    update

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ module.exports = () => {
 
 const useSecureProtocol = (req,res,next) => {
     if(req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production'){
-        return res.redirect('https://'+req.headers.host+req.url)
+        return res.redirect('https://'+'sacovidtracker.online'+req.url)
     }else{
         return next();
     }
