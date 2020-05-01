a312416621588518543f713672d27c3ccdca76d9
Daniel <danielminter13@gmail.com>
2020-05-01 14:42:43 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,6 +2,9 @@ module.exports = () => {
 
 const useSecureProtocol = (req,res,next) => {
     if(req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production'){
+        console.log(`protocol: ${req.headers['x-forwarded-proto']}`)
+        console.log(`header: ${req.headers}`)
+        console.log(`url: ${req.url}`)
         return res.redirect('https://'+req.headers.host+req.url)
     }else{
         return next();
